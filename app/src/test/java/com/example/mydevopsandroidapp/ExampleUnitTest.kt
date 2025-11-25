aefa3a4401532fb6ed58ad3ad030dee31b2f10b4
derrazmed <derrmohamed3@gmail.com>
2025-11-25 23:34:39 +0100

    Failing tests for CI

--- app/src/test/java/com/example/mydevopsandroidapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mydevopsandroidapp/ExampleUnitTest.kt
@@ -7,11 +7,11 @@ import org.junit.Assert.*
 class   ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 1)
+        assertEquals(4, 2 + 5)
     }
 
     @Test
     fun subtract_isCorrect() {
-        assertEquals(4, 8 - 3)
+        assertEquals(4, 8 - 2)
     }
 }
