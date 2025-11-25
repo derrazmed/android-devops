4c0fafbb6613aa00a603e01889b76d7eebfa96eb
derrazmed <derrmohamed3@gmail.com>
2025-11-25 23:28:12 +0100

    Add failing unit tests for CI

--- app/src/test/java/com/example/mydevopsandroidapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mydevopsandroidapp/ExampleUnitTest.kt
@@ -4,14 +4,14 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+class   ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(4, 2 + 1)
+    }
+
+    @Test
+    fun subtract_isCorrect() {
+        assertEquals(4, 8 - 3)
     }
 }
